d326d9b2baf08231fb2c7829e67139020115f743
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-08-17 00:20:58 +0200

    printing a specified variable worked well in the route

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('test')
+const express = require("express");
+const server = express();
+const port = 8001;
+server.listen(port, () => {
+    console.log(`server is running on localhost: ${port}`);
+})
+
+server.get("/", (request, response) => {
+    
+    response.send("Hello World am starting nodejs first homework");
+});
+
+// question1 printing a specified variable in the route by user let say :about.
+
+server.get("/about", (request, response) => {
+    
+    response.send("Hello World we are learning nodejs - about");
+});
+
